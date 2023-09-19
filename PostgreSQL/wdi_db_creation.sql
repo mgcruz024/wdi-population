65cc84a255d81566d55b9035e0360b1fc7dfe3b7
Miguel Cruz <107597357+mgcruz024@users.noreply.github.com>
2023-09-19 15:11:05 -0700

    Delete PostgreSQL directory

--- PostgreSQL/wdi_db_creation.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:5ab48f28210cf564a2024a544854d9578281574d611c363be416d30b99e41b1c
-size 43481
--- PostgreSQL/wdi_sql.sql
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:7982e506f3425f5cbdfd318d81a83b5249aae6d8bec978a1187ed2a24f3b4d75
-size 9648
