e7a261c1dcc20e592732467678e5840eccb088f3
Miguel Cruz <107597357+mgcruz024@users.noreply.github.com>
2023-09-19 21:57:14 -0700

    Update main.sql

--- main.sql
+++ main.sql
@@ -80,9 +80,7 @@ SELECT DISTINCT code FROM indicator;
 SELECT DISTINCT country_code_a3 FROM continent_country
 EXCEPT
 SELECT DISTINCT code FROM country;
--- There are MANY. Will not be able to enforce referential integrity constraits unless 
--- this is fixed
-
+-- There are MANY. Will not be able to enforce referential integrity constraits unless this is fixed.
 
 -- Change the PK in either continent_country or country_data
 -- Check for potential issues:
@@ -106,146 +104,73 @@ ORDER BY country_code_a3;
 
 -- Attempt to reconcile country and country2 without losing any data.
 
----------------------------------------------------------
--- Let's now learn about enforcing referential integrity
----------------------------------------------------------
-
--- Let's take a look at the syntax: https://www.postgresql.org/docs/current/ddl-constraints.html#DDL-CONSTRAINTS-FK
-
--- Before we set the constraints, we need to make a decision what to do
---- ON UPDATE (of the PK)
---- ON DELETE (of the PK)
-
--- The following options are available when deleting or updating references PKs
---- NO ACTION (default, prevents deletion and raises an error)
---- RESTRICT (prevents deletion)
---- CASCADE (deletes or updates referenced values for the entire chain of 1:1 and 1:n relationships)
---- SET NULL (sets the values of FK to NULL)
---- SET DEFAULT (sets the values of FK to the specified default value)
-
--- What about mandatory and optional?
--- You make the PK value mandatory for the FK by (1) enforcing the FK constraints as above and (2) preventing FK fields from accepting NULL
--- To enforce the minimum cardinalty in the other direction you need to use a database trigger (outside of the scope of this module)
-
 ----------------------------------------------------------------
--- Then, let's begin enforcing referential integrity constraints
+------------ Enforcing referential integrity constraints -------
 ----------------------------------------------------------------
 
--- Begin with the continent and continent_country (NO ACTION,NO ACTION)
--- Let's do it by hand first before we use SQL
+-- Enforce continent and continent_country (NO ACTION,NO ACTION)
 
--- Then, let's enforce country2 and continent_country (CASCADE, CASCADE)
--- This time by SQL:
+-- Enforce country2 and continent_country (CASCADE, CASCADE)
 ALTER TABLE public.continent_country
     ADD CONSTRAINT continent_country_fk2 FOREIGN KEY (country_code_a2)
         REFERENCES public.country2 (code) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE;
--- NOTE: PostgreSQL validated this constraint automatically
 
--- Next, let's enforce indicator and country_data (CASCADE, CASCADE)
+-- Enforce indicator and country_data (CASCADE, CASCADE)
 ALTER TABLE public.country_data
     ADD CONSTRAINT country_data_fk2 FOREIGN KEY (indicator_code)
         REFERENCES public.indicator (code) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
 		NOT VALID;	
--- NOTE: PostgreSQL did not validate this one automatically - we need to do it by hand;
--- Let's do it later as it may take some time
 
--- Then, let's enforce country and country_data (CASCADE, CASCADE)
+-- Enforce country and country_data (CASCADE, CASCADE)
 ALTER TABLE public.country_data
     ADD CONSTRAINT country_data_fk1 FOREIGN KEY (country_code)
         REFERENCES public.country (code) MATCH SIMPLE
         ON UPDATE CASCADE
         ON DELETE CASCADE
 		NOT VALID;	
--- NOTE: PostgreSQL did not validate this one automatically - we need to do it by hand
--- Let's do it later as it may take some time
 
--- Finally, let's try to enforce country and continent_country (RESTRICT, RESTRICT)
+-- Enforce country and continent_country (RESTRICT, RESTRICT)
 ALTER TABLE public.continent_country
     ADD CONSTRAINT continent_country_fk3 FOREIGN KEY (country_code_a3)
         REFERENCES public.country (code) MATCH SIMPLE
         ON UPDATE RESTRICT
         ON DELETE RESTRICT
 		NOT VALID;
--- Added, but try to validate this one - it will fail (to be dealt with later in this course)
-
------------------------------------
--- Take a look at the desired model
------------------------------------
-
--- Link: https://www.dropbox.com/s/ran7v2qbwr4a2sn/Module%2007%20ERD%20desired.png?dl=0
-
----------------------------------
--- List all PK and FK constraints
----------------------------------
--- For now for all databases on the server (later we will make it more specific)
-SELECT * FROM pg_catalog.pg_constraint con WHERE contype IN ('p','f');
-
--- Which of them are not validated?
-SELECT * FROM pg_catalog.pg_constraint con WHERE contype IN ('p','f') AND convalidated=false;
-
--- Use DROP CONSTRAINT [name] to remove a constraint
-
--- If you can leave the machine running for some time, 
--- you may try to manually validate the FK1 and FK2 constraints for country data
-
-
--- In this module you have learned to:
--- analyze the existing data model
--- analyze the existing data for potential FK constraint violations
--- enforce referential integrity (FK) constraints of different types with SQL
-
--- =============================================================================================
 
 -------------------------------------------------------
--- Let's now use what we learned to normalize the model
+---------- Normalize the model ------------------------
 -------------------------------------------------------
 
--- Recall the desired model: https://www.dropbox.com/s/ran7v2qbwr4a2sn/Module%2007%20ERD%20desired.png?dl=0
--- This database attempts to combine World Bank indicators with continents
--- We will try to merge country with country2 but country is our primary table
--- So we want to preserve ALL rows in the country table which actually represent countries
-
--- Let's take another look at the tables
+-- Preserve ALL rows in the country table which actually represent countries
 
 SELECT * FROM country; --255 rows but some are NOT countries (e.g. Africa)
 
--- Which countries (names) from our primary table do not have a match in the continent_code?
+-- Which countries (names) from the primary table do not have a match in the continent_code?
 SELECT DISTINCT name, code, country_code_a2 code2
 FROM country LEFT JOIN continent_country ON code=country_code_a3
 WHERE country_code_a2 IS NULL 
 ORDER BY code;
--- Lets export to CSV to take a closer look
 
 -- Only one of these rows is an actual country and it is located in Europe
--- Does this country exist is country2?
+
+-- Does this country exist in country2?
 SELECT * FROM country2 WHERE country LIKE 'Kos%';
--- It actually DOES, so what code does it have in continent_country?
+-- It does
 SELECT * FROM continent_country WHERE country_code_a2 = 'XK';
 
--- What about the three-character codes in continent_country which do not have a matching code in country?
--- What are these?
+-- Three-character codes in continent_country which do not have a matching code in country
 SELECT DISTINCT country_code_a2,country_code_a3,country 
 FROM country RIGHT JOIN continent_country ON country.code=country_code_a3 INNER JOIN country2 on country2.code=country_code_a2
 WHERE country.code IS NULL;
-
--- So we need to: 
--- (a) update the three-character code to KSV for all rows in continent_country
--- (b) delete (or nullify) all entries in continent_country which do not have a three-character match country 
--- (c) remove all non-countries from country - this will cause cascade-deletion of matching country_data rows
--- (d) rename the three-character column in continent_country
--- (e) enforce referential integrity constraints between country and continent_country
--- (f) drop country2 table and country_code_a2 column in continent_country
--- So, let's get to work...
 
 -- (a) update the three-character code to KSV for all rows in continent_country
 UPDATE continent_country SET country_code_a3='KSV' WHERE country_code_a2='XK';
 
 -- (b) delete all entries in continent_country which do not have a three-character match country 
--- Lets use a temporary table (so we do not use the nested queries before we actually cover them)
 SELECT DISTINCT country_code_a2,country_code_a3,country 
 INTO temptable -- Added
 FROM country RIGHT JOIN continent_country ON country.code=country_code_a3 INNER JOIN country2 on country2.code=country_code_a2
@@ -257,8 +182,7 @@ WHERE continent_country.country_code_a3 = temptable.country_code_a3;
 
 DROP TABLE temptable;
 
--- (c) remove all non-countries from country - this will cause cascade-deletion of matching country_data rows
--- Also using a temporary table
+-- (c) remove all non-countries from country 
 SELECT DISTINCT name, code, country_code_a2 code2
 INTO temptable
 FROM country LEFT JOIN continent_country ON code=country_code_a3
@@ -270,7 +194,7 @@ WHERE country.code = temptable.code;
 
 DROP TABLE temptable;
 
--- Check how many countries we have now. NOTE: referential integrity between country and country_data is preserved.
+-- Check how many countries there are now
 SELECT COUNT(*) FROM country;
 SELECT DISTINCT country_code FROM country_data;
 
@@ -278,26 +202,13 @@ SELECT DISTINCT country_code FROM country_data;
 ALTER TABLE public.continent_country
     RENAME country_code_a3 TO country_code;
 	
--- (e) enforce referential integrity constraints between country and continent_country
--- The constraint is already set, we just need to validate it.
--- Which of the constraints are not validated?
-SELECT * FROM pg_catalog.pg_constraint con WHERE contype IN ('p','f') AND convalidated=false;
--- Let's validate it
-ALTER TABLE continent_country
-    VALIDATE CONSTRAINT continent_country_fk3;
-
--- (f) drop country2 table and country_code_a2 column in continent_country
+-- (e) drop country2 table and country_code_a2 column in continent_country
 -- Must drop FK constraint between country2 and continent_country (continent_country_fk2) first
 ALTER TABLE continent_country
 	DROP CONSTRAINT continent_country_fk2;
--- For consistency with the ERD, rename continent_country_fk3 to continent_country_fk2
+-- Rename continent_country_fk3 to continent_country_fk2 (consistent w/ ERD)
 ALTER TABLE continent_country RENAME CONSTRAINT continent_country_fk3 TO continent_country_fk2;
 -- remove country_code_a2 column
 ALTER TABLE continent_country DROP COLUMN country_code_a2;
 -- remove country2 table
 DROP TABLE country2;
-
-
-
-
-
